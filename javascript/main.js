c0d1eb59c83088d3979795795c1c5b4294c45491
dchev001 <dchev001@gmail.com>
2022-09-20 20:16:37 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,14 @@ function calculatePayment()
     pay = document.getElementById("hourly_pay").value;
     hours = document.getElementById("hour_work").value;
 
-    total = (pay * 40) + ((hours - 40) * pay * 1.5);
+	if (hours > 40)
+	{
+        total = (pay * 40) + ((hours - 40) * pay * 1.5);
+	}
+	else
+	{
+		total = (pay * hours);
+	}    
+    
     document.getElementById("info").innerHTML = "<p>Total: $" + total + "</p>";
 }
